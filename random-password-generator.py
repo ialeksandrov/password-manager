dd4a4d2956a78ca32e5932325c31666a5f68abba
ialeksandrov <ialeksandrov1990@abv.bg>
2021-10-01 00:08:33 +0300

    Prepare save functionality

--- random-password-generator.py
+++ random-password-generator.py
@@ -5,17 +5,28 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Process arguments")
 parser.add_argument('--length', dest='length', help='Set the length of the password')
+parser.add_argument('--save', dest='save', help='Save the password')
 args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
-length = int(args.length)
+generated_passwords = 'generated_passwords.txt'
 
+
+length = int(args.length)
 lower = string.ascii_lowercase
 upper = string.ascii_uppercase
 numbers = string.digits
 symbols = string.punctuation
-
 all = lower + upper + numbers + symbols
 temp = random.sample(all, length)
-
 password = "".join(temp)
-print(password)
+with open(generated_passwords, 'a+') as f:
+    f.write(', '.join(f.read().split('\n')) + password + '\n')
+
+
+with open(generated_passwords) as file:
+    saved_passwords = []
+    lines = file.read().split('\n')
+    saved_passwords.append(lines)
+    print(saved_passwords)
+
+
