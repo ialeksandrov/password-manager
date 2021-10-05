41216d6b7f3cda17e6c8bd4ae7af9d48231d0d46
ialeksandrov <ialeksandrov1990@abv.bg>
2021-10-05 22:48:24 +0300

    Fix shadowing of 'all' variable and re-structure the code to use --save command

--- random-password-generator.py
+++ random-password-generator.py
@@ -3,10 +3,6 @@ import random
 import string
 import argparse
 
-parser = argparse.ArgumentParser(description="Password management tool")
-parser.add_argument('--length', dest='length', help='Set the length of the password')
-parser.add_argument('--save', dest='save', help='Save the password')
-args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
 generated_passwords = 'generated_passwords.txt'
 
@@ -17,8 +13,8 @@ def generate_password(length):
     upper = string.ascii_uppercase
     numbers = string.digits
     symbols = string.punctuation
-    all = lower + upper + numbers + symbols
-    temp = random.sample(all, length)
+    full_set = lower + upper + numbers + symbols
+    temp = random.sample(full_set, length)
     password = "".join(temp)
     with open(generated_passwords, 'a+') as f:
         f.write(', '.join(f.read().split('\n')) + password + '\n')
@@ -30,10 +26,21 @@ def save_password():
     with open(generated_passwords, 'r') as file:
         saved_passwords = []
         lines = file.readlines()[-1]
-        saved_passwords.append(lines)
+        saved_passwords.append(lines.strip())
+        print(saved_passwords)
 
         return saved_passwords
 
 
-#print(generate_password(12))
-#print(save_password())
+parser = argparse.ArgumentParser(description="Password management tool")
+parser.add_argument('--length', dest='length', required=True, help='Set the length of the password')
+parser.add_argument('--save', dest='save', help='Save the password')
+subparser = parser.add_subparsers()
+save_parser = subparser.add_parser('save_password', help='Save the generated password.')
+save_parser.set_defaults(func=save_password)
+args = parser.parse_args()
+
+
+print(generate_password(12))
+print(save_password())
+
