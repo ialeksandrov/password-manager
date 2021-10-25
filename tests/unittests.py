b06175f386373d8396d0327dd776d512faec6a11
ialeksandrov <ialeksandrov1990@abv.bg>
2021-10-25 21:59:38 +0300

    Define test cases names

---
+++ tests/unittests.py
@@ -0,0 +1,34 @@
+import unittest
+
+
+class TestPasswordManager(unittest.TestCase):
+
+    def test_remove_duplicate_password(self):
+        pass
+
+    def test_remove_password(self):
+        pass
+
+    def test_generate_password(self):
+        pass
+
+    def test_generate_password_with_custom_length(self):
+        pass
+
+    def test_save_password(self):
+        pass
+
+    def test_list_all_saved_passwords(self):
+        pass
+
+    def test_display_last_saved_password(self):
+        pass
+
+    def test_add_custom_password(self):
+        pass
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
