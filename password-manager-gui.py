8d0f8b74c4d486e3d4947818f9856b15f658010e
ialeksandrov <ialeksandrov1990@abv.bg>
2021-11-23 20:56:44 +0200

    Refactor buttons layout and add textbox

--- password-manager-gui.py
+++ password-manager-gui.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit
 
 
 # Subclass QMainWindow to customize your application's main window
@@ -13,27 +13,31 @@ class MainWindow(QMainWindow):
 
         generate_button = QPushButton("Generate", self)
         generate_button.resize(100, 32)
-        generate_button.move(50, 50)
+        generate_button.move(170, 300)
 
         save_button = QPushButton("Save", self)
         save_button.resize(100, 32)
-        save_button.move(50, 100)
+        save_button.move(290, 300)
 
         list_saved_button = QPushButton("List saved", self)
         list_saved_button.resize(100, 32)
-        list_saved_button.move(50, 150)
+        list_saved_button.move(410, 300)
 
         list_all_button = QPushButton("List all", self)
         list_all_button.resize(100, 32)
-        list_all_button.move(50, 200)
+        list_all_button.move(530, 300)
 
         remove_button = QPushButton("Remove", self)
         remove_button.resize(100, 32)
-        remove_button.move(50, 250)
+        remove_button.move(650, 300)
 
         add_custom_button = QPushButton("Add", self)
         add_custom_button.resize(100, 32)
         add_custom_button.move(50, 300)
+
+        self.textbox = QLineEdit(self)
+        self.textbox.move(50, 350)
+        self.textbox.resize(700, 200)
 
         self.setFixedSize(QSize(800, 600))
 
