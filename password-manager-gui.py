d2b22a23f0aa23c5e01dddfd74c79c4433004a86
ialeksandrov <ialeksandrov1990@abv.bg>
2021-11-25 11:45:24 +0200

    Adjusting size and position.

--- password-manager-gui.py
+++ password-manager-gui.py
@@ -13,33 +13,33 @@ class MainWindow(QMainWindow):
 
         self.generate_button = QPushButton("Generate", self)
         self.generate_button.resize(100, 32)
-        self.generate_button.move(170, 300)
+        self.generate_button.move(170, 50)
 
         self.save_button = QPushButton("Save", self)
         self.save_button.resize(100, 32)
-        self.save_button.move(290, 300)
+        self.save_button.move(290, 50)
 
         self.list_saved_button = QPushButton("List saved", self)
         self.list_saved_button.resize(100, 32)
-        self.list_saved_button.move(410, 300)
+        self.list_saved_button.move(410, 50)
 
         self.list_all_button = QPushButton("List all", self)
         self.list_all_button.resize(100, 32)
-        self.list_all_button.move(530, 300)
+        self.list_all_button.move(530, 50)
 
         self.remove_button = QPushButton("Remove", self)
         self.remove_button.resize(100, 32)
-        self.remove_button.move(650, 300)
+        self.remove_button.move(650, 50)
 
         self.add_custom_button = QPushButton("Add", self)
         self.add_custom_button.resize(100, 32)
-        self.add_custom_button.move(50, 300)
+        self.add_custom_button.move(50, 50)
 
         self.textbox = QLineEdit(self)
-        self.textbox.move(50, 350)
+        self.textbox.move(50,   100)
         self.textbox.resize(700, 200)
 
-        self.setFixedSize(QSize(800, 600))
+        self.setFixedSize(QSize(800, 350))
 
 
 app = QApplication(sys.argv)
