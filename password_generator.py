e4ede7125652006335236921ac59248dc5b59cc6
ialeksandrov <ialeksandrov1990@abv.bg>
2024-04-24 22:38:18 +0300

    Refactoring the way passwords are stored

--- password_generator.py
+++ password_generator.py
@@ -2,9 +2,22 @@ import sys
 import random
 import string
 import argparse
+import sqlite3
 
-generated_passwords = 'generated_passwords.txt'
-saved_passwords = 'saved_passwords.txt'
+
+conn = sqlite3.connect('password_manager.db')
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        username TEXT,
+        password TEXT
+    )
+''')
+
+conn.commit()
+cursor.close()
 
 
 def generate_password(length):
@@ -16,96 +29,17 @@ def generate_password(length):
     full_set = lower + upper + numbers + symbols
     temp = random.sample(full_set, length)
     password = "".join(temp)
-    with open(generated_passwords, 'a+') as f:
-        f.write(', '.join(f.read().split('\n')) + password + '\n')
 
     return password
 
 
-def save_password():
-    with open(generated_passwords, 'r') as file:
-        with open(saved_passwords, 'a') as f:
-            lines = file.readlines()[-1]
-            f.write(lines)
+def create_password(title, username, password):
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO users (title, username, password) VALUES (?, ?, ?)', (title, username, password))
+    conn.commit()
+    cursor.close()
 
-    return lines
-
-
-def list_saved_password():
-    converted_list = []
-    with open(saved_passwords, 'r') as fp:
-        lines = fp.readlines()
-        for line in lines:
-            converted_list.append(line.strip())
-
-    return converted_list
+    return title, username, password
 
 
-def list_saved():
-    last_saved = []
-    with open(saved_passwords, 'r') as fp:
-        lines = fp.readlines()[-1]
-        last_saved.append(lines.strip())
-
-    return last_saved
-
-
-def remove_password(password):
-    result = []
-    with open(saved_passwords, 'r') as file:
-        for lines in file:
-            result.append(lines.strip())
-
-    for items in result:
-        if args.password in result:
-            result.remove(args.password)
-
-    with open(saved_passwords, 'w') as f:
-        for items in result:
-            f.write(items + '\n')
-
-    return result
-
-
-def add_custom_password(password):
-    password = args.add
-    with open(saved_passwords, 'a') as f:
-        f.write(password + '\n')
-
-    return password
-
-
-parser = argparse.ArgumentParser(description="Password management tool")
-parser.add_argument('--length', dest='length', help='Set the length of the password')
-parser.add_argument('--remove', dest='password', help='Remove old or unused passwords')
-parser.add_argument('--add', dest='add', help='Add custom password to saved passwords')
-sub_parser = parser.add_subparsers()
-save_parser = sub_parser.add_parser('save_password', help='Save the generated password')
-list_parser = sub_parser.add_parser('list', help='List all saved passwords.')
-list_last_parser = sub_parser.add_parser('list_saved', help='Show the last saved password')
-save_parser.set_defaults(func=save_password)
-list_parser.set_defaults(func=list_saved_password)
-list_last_parser.set_defaults(func=list_saved)
-if len(sys.argv) == 1:
-    parser.print_help(sys.stderr)
-    sys.exit(1)
-args = parser.parse_args()
-
-
-if sys.argv[1] == '--length':
-    print(generate_password(args.length))
-
-if sys.argv[-1] == 'save_password':
-    print(save_password())
-
-if sys.argv[-1] == 'list':
-    print(list_saved_password())
-
-if sys.argv[-1] == 'list_saved':
-    print(list_saved())
-
-if sys.argv[1] == '--remove':
-    print(remove_password(args.password))
-
-if sys.argv[1] == '--add':
-    print(add_custom_password(args.password))
+#create_password('os creds', 'ivan', 'sSSxcgW#3232Sa')
